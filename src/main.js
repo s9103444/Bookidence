4d3166d458e2f6b2dffc7a088884fdf1b5fd6b49
hannahandnicle <hannahandnicle@gmail.com>
2026-08-03 18:24:17 +0800

    新增12欄位

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
 import "./assets/scss/all.scss";
+import '@/assets/scss/main.scss'
 createApp(App).use(router).mount("#app");
--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -19,14 +19,18 @@ import AppIcon from "@/components/common/AppIcon.vue";
     </div>
   </header>
 
-  <section class="intro-homeroom">
-    <div class="content-homeroom-intro">
+
+<!-- 1. 加上 container 啟用 12 欄 Grid -->
+  <section class="intro-homeroom container">
+<!-- 左側內容區：佔 6 欄 -->
+    <div class=" col-6 content-homeroom-intro">
     <p class="tagline-homeroom-intro" >搭建一座屬於你的閱讀小屋</p>
     <h3 class="title-homeroom-intro" >每一次翻頁，<br>都為小屋砌上一片磚瓦</h3>
     <p class="desc-homeroom-intro">這不只是個冷冰冰的數位書棚，<br>而是你在 Bookidence 小鎮裡親手搭建的精神樹屋。<br>讓閱讀累積的足跡，化作肉眼可見的溫暖裝飾。</p>
     <AppButton >進入我的書房 <AppIcon name="arrow-right" /></AppButton>
     </div>
-    <div class="img-homeroom-intro">
+    <!-- 右側圖片區：佔 6 欄 -->
+    <div class=" col-6 img-homeroom-intro">
       <img src="/src/assets/images/home-element/house.png" alt="" style="width: 100%";>
     </div>
   </section>
@@ -54,7 +58,7 @@ import AppIcon from "@/components/common/AppIcon.vue";
 
 <style lang="scss" scoped>
 @use "../../assets/scss/abstracts/variables" as *;
-@use "../../assets/scss/abstracts/mixins" as *;
+
 
 
 .kv-section-img{
@@ -71,7 +75,7 @@ import AppIcon from "@/components/common/AppIcon.vue";
 }
 .kv-welcome{
 
-  display: flex;
+  text-align: center;
   justify-content: center;
   color: $primary;
   font-size:$p-md-size;
@@ -81,7 +85,7 @@ import AppIcon from "@/components/common/AppIcon.vue";
 }
 
 .kv-title{
-   display: flex;
+  text-align: center;
   justify-content: center;
   font-size:$h1-size;
   color: $primary;
@@ -99,20 +103,16 @@ import AppIcon from "@/components/common/AppIcon.vue";
 }
 
 .intro-homeroom{
-  @include grid-container; // 載入 Count: 12, Gutter: 24px, Margin: 48px
-  align-items: center;    // 控制 Grid 內的子元素垂直居中（取代 display: flex）
+  
+  align-items: center;   // 讓 6 欄與 6 欄之間的內容垂直居中
   background-image: url("@/assets/images/home-element/light-green-pixel.png");
   background-repeat: no-repeat;
   background-size: cover;
-  align-items: center;
- 
   
-
 }
 
 
 .content-homeroom-intro{
-  @include col-span(6); // 佔據 6 欄
   padding-top:120px ;
   
 }
@@ -125,6 +125,7 @@ import AppIcon from "@/components/common/AppIcon.vue";
 
 }
 
+
 .title-homeroom-intro{
   font-size: $h1-size;
   color:$neutral-800;
@@ -132,16 +133,18 @@ import AppIcon from "@/components/common/AppIcon.vue";
   margin-bottom: 64px;
 
 }
-
-
 .desc-homeroom-intro{
   font-size:$p-md-size;
   margin-bottom: 64px;
 }
 
+
+
+
 .img-homeroom-intro{
-  @include col-span(6); // 佔據 6 欄
   padding-top:120px ;
+  display: flex;
+  justify-content: center;
 
 }
 </style>
