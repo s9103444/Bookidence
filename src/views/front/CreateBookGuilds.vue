efd95936eb1f9b6b816daeccbe75817ad08e5115
hannahandnicle <hannahandnicle@gmail.com>
2026-08-11 09:45:24 +0800

    建立 最新消息、建立讀書公會頁面

--- src/views/front/CreateBookGuilds.vue
+++ src/views/front/CreateBookGuilds.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div class="create-book-guilds">
     <h1>建立讀書公會</h1>
     <!-- 你的內容放這裡 -->
@@ -20,13 +19,3 @@ export default {
 <style scoped>
 /* 你的樣式放這裡 */
 </style>
-=======
-  <div></div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style lang="scss" scoped></style>
->>>>>>> 6f0fe0f3df2a5bb2d1dab5fb3fa2599d0d527155
--- src/views/front/NewsView.vue
+++ src/views/front/NewsView.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div >
     <h1>最新消息</h1>
     <!-- 你的內容放這裡 -->
@@ -20,13 +19,3 @@ export default {
 <style scoped>
 /* 你的樣式放這裡 */
 </style>
-=======
-  <div></div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style lang="scss" scoped></style>
->>>>>>> 6f0fe0f3df2a5bb2d1dab5fb3fa2599d0d527155
