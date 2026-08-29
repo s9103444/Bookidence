171958734f707dea5d20d07f4a8d8a9f9217042e
s9103444 <s9103444@gmail.com>
2026-08-29 18:51:22 +0800

    add photosticker API update bookroom

---
+++ src/components/front/PhotoSticker.vue
@@ -0,0 +1,18 @@
+<template>
+  <canvas ref="avatarCanvas"></canvas>
+</template>
+
+<script setup>
+import { ref, toRef } from "vue";
+import { useDrawAvatar } from "../../composables/useDrawAvatar";
+const props = defineProps({
+  userId: { type: [String, Number], required: true },
+  width: { type: Number, default: 64 },
+});
+
+const avatarCanvas = ref(null);
+const userIdRef = toRef(props, "userId");
+useDrawAvatar(avatarCanvas, userIdRef, props.width);
+</script>
+
+<style lang="scss" scoped></style>
