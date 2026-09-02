8dc49028e3d8fed367e4ca6b56b1f4f415852a0d
hannahandnicle <hannahandnicle@gmail.com>
2026-09-02 18:23:48 +0800

    新增-後台會員管理串接

--- src/common/php/admin_members.php
+++ src/common/php/admin_members.php
@@ -11,7 +11,30 @@
     $stmt->execute();
     $members=$stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    echo json_encode(['success'=>true,'member'=>$members],JSON_UNESCAPED_UNICODE);
+    $stmt=$pdo->prepare("
+    SELECT action_id,target_user_id,staff_account,report_id,
+    action_type,reason,created_at,revoked_at,revoked_by
+    FROM moderation_action
+   
+
+    ");
+    $stmt->execute( );
+    $actions=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    $stmt=$pdo->prepare("
+    SELECT guildmember.user_id,guildmember.guild_id,guildmember.permission_level,guildmember.member_status,guild.guild_name
+    FROM guildmember
+    JOIN guild ON guild.guild_id=guildmember.guild_id
+    WHERE member_status='在會中'
+    ");
+    $stmt->execute( );
+    $inGuilds=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
+    echo json_encode(['success'=>true,'member'=>$members,'inGuild'=>$inGuilds,'action'=>$actions],JSON_UNESCAPED_UNICODE);
 
     
 
